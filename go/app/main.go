968e27a2529aa584b7604dd4aa624dcb7959d1eb
mizutanimeen <dongxingshuigu@gmail.com>
2023-03-12 18:51:30 +0900

    add: 最低限のgoサーバー,docker

---
+++ go/app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	tRouter := chi.NewRouter()
+	tRouter.Use(middleware.Logger)
+	tRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	http.ListenAndServe(":8080", tRouter)
+}
